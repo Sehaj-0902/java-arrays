878476b105ec491c1eddbb285e57792200ddd07f
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-03-04 10:20:29 +0530

    [Sehaj] ADD: Program to check if a number is positive, negative or zero using an array and compare the first and last element of the array

--- PositiveNegativeZero.java
+++ PositiveNegativeZero.java
@@ -1,5 +1,6 @@
 // Create PositiveNegativeZero class to check if a number is positive, negative or zero and using an array to store elements
 // If the number is positive then check if the number is even or odd
+// Compare the first and last element of array to check if they are greater, less or equal
 
 // Import Scanner class from java.util package
 import java.util.Scanner;
@@ -9,7 +10,7 @@ public class PositiveNegativeZero {
 		// Create a Scanner object
 		Scanner sc = new Scanner(System.in);
 
-		// Define an array elements of size 5
+		// Declare and initialize an array elements of size 5
 		int[] elements = new int[5];
 
 		// Store input elements in array
@@ -35,6 +36,17 @@ public class PositiveNegativeZero {
 			else {
 				System.out.println("The number is Zero");
 			}
+		}
+
+		// Comparing first and last element of array to check if they are greater, less or equal
+		if (elements[0] > elements[elements.length-1]) {
+			System.out.println("The first element is Greater than the last element of array");
+		}
+		else if (elements[0] < elements[elements.length-1]) {
+			System.out.println("The first element is Less than the last element of array");
+		}
+		else {
+			System.out.println("The first element and the last element of array are Equal");
 		}
 	}
 }
